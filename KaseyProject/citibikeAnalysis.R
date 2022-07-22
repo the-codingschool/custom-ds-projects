2ce71b1ddd91d7b701463722461617dc5c00e3df
Kasey Wang <kaseyywang@gmail.com>
2022-07-22 11:42:28 -0700

    made new file for data analysis

---
+++ KaseyProject/citibikeAnalysis.R
@@ -0,0 +1,12 @@
+# load in required libraries
+library(dplyr)
+library(ggplot2)
+
+# filtering data by ridable_types
+
+## cluster classic bikes by latitude and longitude
+
+## cluster electric bikes by latitude and longitude
+
+
+# 
